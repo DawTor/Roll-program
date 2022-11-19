5d321f64d3937cae3c72f2bc695838d0443918b8
DawTor <111532769+DawTor@users.noreply.github.com>
2022-11-19 17:06:56 +0100

    Update roller.py

--- roller.py
+++ roller.py
@@ -3,48 +3,48 @@ from tkinter import *
 import random
 from tkinter.messagebox import showinfo
 
-listofelements = []
+list_of_elements = []
 
 def add_button():
     element = entrybox.get()
     entrybox.delete(0, END)
 
     if element:
-        with open("elementslist.txt","a") as elf:
+        with open("elementslist.txt", "a") as elf:
             elf.write(f'\n{element}')
-        listofelements.append(element)
+        list_of_elements.append(element)
         listbox.insert(END, element)
 
+        
 def del_button():
-
     element = str(listbox.get(ANCHOR))
-    if element in listofelements:
-        listofelements.remove(element)
-        with open("elementslist.txt","w") as elf:
-            for element in listofelements:
+    if element in list_of_elements:
+        list_of_elements.remove(element)
+        with open("elementslist.txt", "w") as elf:
+            for element in list_of_elements:
                 elf.write(element+'\n')
 
         listbox.delete( ANCHOR)
 
+        
 def roll_button():
-    choice = random.choice(listofelements)
+    choice = random.choice(list_of_elements)
     showinfo(title='Roll Winner!', message=f'{choice} WON!')
 
+    
 def listfile():
-
     try:
-        with open("elementslist.txt","r") as elf:
+        with open("elementslist.txt", "r") as elf:
             elements = elf.readlines()
 
         for element in elements:
             if element !="\n":
-                listofelements.append(element)
+                list_of_elements.append(element)
                 listbox.insert(END ,element)
 
     except:
-        file = open("elementslist.txt","w")
+        file = open("elementslist.txt", "w")
         file.close()
-
 
 
 #create a window
@@ -52,9 +52,9 @@ window = Tk()
 window.title("Roller program")
 window.geometry("400x220")
 window.resizable(False, False)
-window.config(bg="#211f1f")
+window.config(bg="#252525")
 
-frame = Frame(window, bg="#211f1f")
+frame = Frame(window, bg="#252525")
 frame.place(x=48,y=20)
 
 listbox = Listbox(frame, height=5, width=50,)
@@ -67,9 +67,9 @@ entrybox.focus()
 
 listfile()
 
-addbutton = Button(frame, text="Add", height=3, width=10,command=add_button).pack(side="left")
-delbutton = Button(frame, text="Delete", height=3, width=10,command=del_button ).pack(side="left")
-rollbutton = Button(frame, text="Roll", height=3, width=10,command=roll_button ).pack(side="right")
+addbutton = Button(frame, text="Add", height=3, width=10, command=add_button).pack(side="left")
+delbutton = Button(frame, text="Delete", height=3, width=10, command=del_button ).pack(side="left")
+rollbutton = Button(frame, text="Roll", height=3, width=10, command=roll_button ).pack(side="right")
 
 
 window.mainloop()
